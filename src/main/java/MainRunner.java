3c247b0a1c783b44d188267c024d1cd676a5fa97
Suhas <imsuhas11@gmail.com>
2023-04-25 15:56:11 +0530

    Main runner modified

--- src/main/java/MainRunner.java
+++ src/main/java/MainRunner.java
@@ -1,21 +1,13 @@
 public class MainRunner {
 
     public static void main(String[] args) {
-       /* RecentlyPlayedSongs recentlyPlayedSongs = new RecentlyPlayedSongs(3);
-        recentlyPlayedSongs.addSong("s1","User1");
-        recentlyPlayedSongs.addSong("s2","User1");
-        recentlyPlayedSongs.addSong("s3","User1");
-        recentlyPlayedSongs.addSong("s4","User1");
-
-        System.out.println(recentlyPlayedSongs.getRecentlyPlayedSongs("User1"));*/
-
         RecentlyPlayedSong recentlyPlayedSong = new RecentlyPlayedSong(3);
-        recentlyPlayedSong.add("User1",new Song("S1"));
-        recentlyPlayedSong.add("User1",new Song("S2"));
-        recentlyPlayedSong.add("User1",new Song("S3"));
-        recentlyPlayedSong.add("User1",new Song("S4"));
+        recentlyPlayedSong.add("User1", "S1");
+        recentlyPlayedSong.add("User1", "S2");
+        recentlyPlayedSong.add("User1", "S3");
+        recentlyPlayedSong.add("User1", "S4");
 
-        recentlyPlayedSong.getRecentlyPlayed("User1").stream().forEach(s-> System.out.println(s.getName()));
+        recentlyPlayedSong.getRecentlyPlayed("User1").stream().forEach(System.out::println);
 
     }
 }
